5737d57ce0e9283a462d296ad091a9d47b3b28a5
Eliadad <eliasashtonsaunders@gmail.com>
2021-05-22 11:28:13 -0700

    added all questions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { validate } = require('@babel/types');
+
 
 const questions = [
     {
@@ -16,7 +16,55 @@ const questions = [
             }
         }
     }, {
+        type: "input",
+        name: "id",
+        message: "Enter your employee ID"
+    }, {
+        type: "input",
+        name: "email",
+        message: "Enter your email"
+    }, {
+        type: "input",
+        name: "office number",
+        messsage: "Enter your office number:"
+    } ]
 
-    }]
+const addQuestion = {
+    type: "list",
+    name: "choicing",
+    message: "Do you want to add employees?",
+    choices: ["Engineer", "Intern", "End addtions"]
+};
 
-inquirer.prompt(questions).then(console.log(""))
+const promptArr = [...questions, addQuestion]
+
+const internQuestions = [{
+    type: "input",
+    name: "intern-name",
+    message: "Enter interns name:"
+}, {
+    type: "input",
+    name: "intern-id",
+    message: "Enter intern ID:"
+}, {
+    type: "input",
+    name: "intern-github",
+    message: "Enter interns gihub:"
+} ]
+
+const engineerQuestions = [{
+    type: "input",
+    name: "engineer-name",
+    message: "Enter engineers name:"
+}, {
+    type: "input",
+    name: "engineer-id",
+    message: "Enter engineers ID:"
+}, {
+    type: "input",
+    name: "engineer-github",
+    message: "Enter engineers gihub:"
+} ]
+
+inquirer.prompt(promptArr).then()
+    
